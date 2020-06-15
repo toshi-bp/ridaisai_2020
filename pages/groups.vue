07c5cd5c869d384896982fb096750f40138a27eb
Toshi <yttoshi0919@gmail.com>
2020-06-15 19:33:01 +0900

    新しい資料をアップロード

--- pages/groups.vue
+++ pages/groups.vue
@@ -37,7 +37,7 @@
               参加団体向け資料<small>（6月15日更新）</small>
             </h2>
             <ul>
-              <li> <a href="2020前期配布資料完成版.pdf" target="_blank">2020年度前期配布資料(PDF版)</a></li>
+              <li> <a href="2020前期配布資料.pdf" target="_blank">2020年度前期配布資料(PDF版)</a></li>
               <li> <a href="2020規約書.pdf" target="_blank">2020年度参加団体規約書(PDF版)</a></li>
             </ul>
             <h2 class="groups__title">
