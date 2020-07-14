305a83f09fa1a4375aca7fb9a07da62b5022456e
Toshi <yttoshi0919@gmail.com>
2020-07-14 23:03:21 +0900

    英語の日付表記をミスってた

--- components/organisms/HomeHead.vue
+++ components/organisms/HomeHead.vue
@@ -13,7 +13,7 @@
             <p class="head__main__lead">
               <span>Tokyo University of Science</span><br>
               <span>TUS festival @ Noda</span><br>
-              <span>2020 November 21st,22nd 10:00～17:00</span>
+              <span>November 21st,22nd 2020 10:00～17:00</span>
             </p>
           </div>
           <div class="button__group">
