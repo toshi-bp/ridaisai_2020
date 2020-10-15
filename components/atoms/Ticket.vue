a666c99bf7459b4288f5455b6e9d364dfdebf849
toshi-bp <yttoshi-bp@outlook.jp>
2020-10-15 22:34:13 +0900

    次回スマホ版の位置調整及びスタンプの位置調整 #7

---
+++ components/atoms/Ticket.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="ticket">
+    <div class="ticket__upperline">
+      ONLINE RIDAISAI
+    </div>
+    <div class="ticket__main">
+      <div class="ticket__main__left">
+        <h2 class="ticket__title">
+          オンライン理大祭の歩き方
+        </h2>
+        <h2 class="ticket__description">
+          オンライン理大祭には次の3つの楽しみ方があります。<br />それぞれについて知って理大祭を楽しもう!<br />このページを読んで気になる企画をCHECK!
+        </h2>
+        <p class="ticket__description">
+          企画一覧はこちら
+        </p>
+        <div>
+          <LinkButton to="/" class="ticket__button">YouTubeで楽しむ</LinkButton>
+          <LinkButton to="/" class="ticket__button">Webサイトで楽しむ</LinkButton>
+          <LinkButton to="/" class="ticket__button">Live配信で楽しむ</LinkButton>
+        </div>
+      </div>
+      <div class="ticket__main__right">
+        <img src="~@/assets/image/stamp.svg" class="ticket__main__right__stamp"/>
+      </div>
+    </div>
+    <div class="ticket__bottomline">
+    </div>
+  </div>
+</template>
+
+<script>
+import LinkButton from '~/components/atoms/LinkButton.vue'
+
+export default {
+  components: {
+    LinkButton
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.ticket {
+  width: 100%;
+  margin: 4rem auto;
+  background-color: #fff;
+
+  &__upperline{
+      height: 1.5rem;
+      padding: 0.25rem 0.5rem;
+      background-color: $theme-color;
+      color: #fff;
+      font-size: 1rem;
+      font-family: $sub-font;
+  }
+  &__main{
+      display: grid;
+      grid-template-columns: 80% 1fr;
+      padding: 2rem 1rem;
+      background-color: #fff;
+    &__left{
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        margin: 0;
+    }
+    &__right{
+        display: flex;
+        margin: 0;
+        border-left: dashed 5px $muted;
+        text-align: right;
+        &__stamp {
+          width: 90%;
+        }
+    }
+  }
+  &__title{
+      font-size: 2rem;
+  }
+  &__description{
+      font-size: 1.2rem;
+      margin-bottom: 1rem;
+  }
+  &__button{
+      width: 30%;
+      font-size: 1rem;
+  }
+  &__bottomline{
+      height: 1rem;
+      background-color: $theme-color;
+  }
+}
+</style>
---
+++ pages/welcome.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="welcome__bg">
+    <TheContainer>
+      <div class="welcome__ticket">
+        <img src="~@/assets/image/ticket.svg"  class="welcome__ticket__body"/>
+        <Ticket />
+        <div>
+          <LinkButton to="/">Webサイトの企画はこちら</LinkButton>
+        </div>
+        <div>
+          <LinkButton2 to="/">Live配信の企画はこちら</LinkButton2>
+        </div>
+        <div>
+          <LinkButton3 to="/">Webサイトの企画はこちら</LinkButton3>
+        </div>
+      </div>
+    </TheContainer>
+  </div>
+</template>
+
+<script>
+import TheContainer from '~/components/atoms/TheContainer.vue'
+import Ticket from '~/components/atoms/Ticket.vue'
+import LinkButton from '~/components/atoms/LinkButton.vue'
+import LinkButton2 from '~/components/atoms/LinkButton2.vue'
+import LinkButton3 from '~/components/atoms/LinkButton3.vue'
+
+export default {
+  components: {
+    TheContainer,
+    Ticket,
+    LinkButton,
+    LinkButton2,
+    LinkButton3
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.welcome {
+  &__bg {
+    padding-top: $global-header-height;
+    padding-bottom: 2rem;
+    background-image: url('~@/assets/image/bg.svg');
+    background-size: repeat;
+  }
+  &__ticket {
+    padding-top: 2rem;
+    width: 100%;
+    &__body {
+      user-select: none;
+    }
+  }
+}
+</style>
