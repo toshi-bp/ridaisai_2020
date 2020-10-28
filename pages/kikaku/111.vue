bbdad3075b1fa4d71cd4a9395aa3739dfc0e9ac0
toshi-bp <yttoshi-bp@outlook.jp>
2020-10-29 01:03:33 +0900

    鉄道研究会 #5

--- pages/kikaku/111.vue
+++ pages/kikaku/111.vue
@@ -43,6 +43,7 @@ import TheSection from '~/components/atoms/TheSection'
 import TheContainer from '~/components/atoms/TheContainer'
 
 import KikakuList from '~/kikaku/KikakuList.json'
+import makeHead from '~/utils/makeHead.js'
 
 export default {
   components: {
@@ -62,6 +63,13 @@ export default {
       const id = 111 // kikaku_idの値をjsonから調べて直接入力
       return KikakuList.filter(item => item.kikaku_id === id)
     }
+  },
+  head () {
+    return makeHead(
+      'Space Delivery',
+      '建築サークルDOC',
+      require('~/assets/kikaku/GD0DcS7jHCVS5fr7t9p83BGU4q3Wxfg7Wvjju40V.jpeg')
+    )
   }
 }
 </script>
