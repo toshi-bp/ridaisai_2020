e33b339d41c8fe04ecff621fa86657553fe6478d
toshi-bp <yttoshi-bp@outlook.jp>
2020-10-31 14:49:26 +0900

    とりあえずライブ配信ページの下地は完成か... #11

--- components/organisms/LiveKikaku.vue
+++ components/organisms/LiveKikaku.vue
@@ -8,8 +8,41 @@
       </SubHeader>
       <div>
         <TheSection>
+          <p>ここではライブ配信を行っています。配信は二つの会場で行なわれています。</p>
           <!-- ここにライブ配信する動画を埋め込むよ(チャットを載せるか否かは要相談) -->
+           <div class="live__movie">
+             <!-- 公開に際しチャットを埋め込む場合はsrcのurlの部分を変更すること -->
+              <youtube
+                ref="youtube"
+                :video-id="videoId_1"
+                :fit-parent="true"
+                :resize="true"
+              />
+              <iframe
+                height="400"
+                frameborder="0"
+                :src="`https://www.youtube.com/live_chat?v=${videoId_1}&embed_domain=localhost`"
+                allowfullscreen
+              >
+                <!--チャットの高さについては要調整(特にスマホ版)あとURLも-->
+              </iframe>
+              <youtube
+                ref="youtube"
+                :video-id="videoId_2"
+                :fit-parent="true"
+                :resize="true"
+              />
+              <iframe
+                height="400"
+                frameborder="0"
+                :src="`https://www.youtube.com/live_chat?v=${videoId_2}&embed_domain=localhost`"
+                allowfullscreen
+              />
+            </div>
         </TheSection>
+      </div>
+      <div>
+        <TimeTable />
       </div>
       <div>
         <LinkButton
@@ -27,22 +60,45 @@ import TheSection from '~/components/atoms/TheSection.vue'
 import TheContainer from '~/components/atoms/TheContainer.vue'
 import SubHeader from '~/components/organisms/SubHeader.vue'
 import LinkButton from '~/components/atoms/LinkButton.vue'
+import TimeTable from '~/components/molecules/TimeTable'
 
 export default {
   components: {
     TheSection,
     TheContainer,
     SubHeader,
-    LinkButton
+    LinkButton,
+    TimeTable
   },
   data () {
     return {
-      videoId: '1111111' // YouTubeの動画IDを入力してね
+      videoId_1: 'YAdpFRgXjh0',
+      videoId_2: 'CiEzaiFuz9E' // 動画IDを入力する
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.live {
+  padding-top: $global-header-height;
+  padding-bottom: 2rem;
+  background-image: url('~@/assets/image/bg.svg');
+  background-size: repeat;
 
+  &__movie {
+    &__live {
+      width: 100%;
+      &__body {
+        height: 35rem;
+        @include media-breakpoint-down(md) {
+          height: 40rem;
+        }
+        @include media-breakpoint-down(sm) {
+          height: 30rem;
+        }
+      }
+    }
+  }
+}
 </style>
---
+++ pages/kikaku/live.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <LiveKikaku />
+  </div>
+</template>
+
+<script>
+import LiveKikaku from '~/components/organisms/LiveKikaku'
+
+export default {
+  components: {
+    LiveKikaku
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
