9248acc2daba8340918e561f8a46f557ad8e7d5f
toshi-bp <yttoshi-bp@outlook.jp>
2020-11-04 14:48:38 +0900

    企画をランダム表示できるようにする #13

--- components/molecules/HomeBody.vue
+++ components/molecules/HomeBody.vue
@@ -14,10 +14,29 @@
       <div>
         <nuxt-link to="/kikaku/">
           <div>
-            <h4>企画一覧</h4>
+            <h4>企画を楽しむ</h4>
           </div>
         </nuxt-link>
-        <TheColumn></TheColumn>
+        <TheRow>
+          <TheColumn
+            v-for="item in KikakuList"
+            :key="item.kikaku_id"
+            :spsize="12"
+            :pcsize="6"
+          >
+            <ItemCard
+              :to="`/kikaku/${item.kikaku_id}/`"
+              :label-type="item.type"
+              :label-text="item.type"
+              :title="item.kikaku_title"
+              :name="item.name"
+              :image-url="item.image_filename ? require(`~/assets/kikaku/${item.image_filename}`) : ''"
+              :live="item.live"
+              :youtube="item.youtube"
+              :website="item.website"
+            ></ItemCard>
+          </TheColumn>
+        </TheRow>
       </div>
     </div>
   </div>
@@ -25,10 +44,16 @@
 
 <script>
 import TheColumn from '~/components/atoms/TheColumn'
+import TheRow from '~/components/atoms/TheRow'
+import ItemCard from '~/components/molecules/ItemCard'
+
+import KikakuList from '~/kikaku/KikakuList.json'
 
 export default {
   components: {
-    TheColumn
+    TheColumn,
+    TheRow,
+    ItemCard
   }
 }
 </script>
