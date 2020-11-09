2ec953a88dbccd9c630167220012f38877bfcd7f
toshi-bp <yttoshi-bp@outlook.jp>
2020-11-09 19:04:03 +0900

    吹奏楽部も一応完了 #9

--- pages/kikaku/151.vue
+++ pages/kikaku/151.vue
@@ -20,7 +20,22 @@
         <TheContainer>
           <TheSection>
             <h3>企画紹介</h3>
-            {{ KikakuList.description }}
+            <p>{{ KikakuList.description }}</p>
+            <div>
+              <LinkToContents
+                :live="true"
+                startTime="17:30"
+                endTime="18:00"
+              ></LinkToContents>
+            </div>
+            <div class="kikaku-info__youtube">
+              <youtube
+                ref="youtube"
+                :video-id="videoId"
+                :fit-parent="true"
+                :resize="true"
+              />
+            </div>
           </TheSection>
         </TheContainer>
 
@@ -41,16 +56,23 @@ import KikakuInfoHeader from '~/components/molecules/KikakuInfoHeader'
 import KikakuInfoBody from '~/components/molecules/KikakuInfoBody'
 import TheSection from '~/components/atoms/TheSection'
 import TheContainer from '~/components/atoms/TheContainer'
+import LinkToContents from '~/components/atoms/LinkToContents'
 
 import KikakuList from '~/kikaku/KikakuList.json'
 import makeHead from '~/utils/makeHead.js'
 
 export default {
+  data () {
+    return {
+      videoId: 'a8_CWDB1mLQ'
+    }
+  },
   components: {
     KikakuInfoHeader,
     KikakuInfoBody,
     TheSection,
-    TheContainer
+    TheContainer,
+    LinkToContents
   },
   props: {
     id: {
@@ -80,5 +102,8 @@ export default {
   padding-bottom: 2rem;
   background-image: url('~@/assets/image/bg.svg');
   background-size: repeat;
+  &__youtube {
+    margin: 3rem 0 1.5rem;
+  }
 }
 </style>
