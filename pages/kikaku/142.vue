97fc95b01ec69e60f66b2ba38d11f88bda49a5a3
toshi-bp <yttoshi-bp@outlook.jp>
2020-11-11 16:03:37 +0900

    クラスの付与

--- pages/kikaku/142.vue
+++ pages/kikaku/142.vue
@@ -20,7 +20,7 @@
         <TheContainer>
           <TheSection>
             <h3>企画紹介</h3>
-            <p>{{ KikakuList.description }}</p>
+            <p class="kikaku-info__description">{{ KikakuList.description }}</p>
           </TheSection>
         </TheContainer>
 
