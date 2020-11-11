734fd6fe2ddbf09e5c246a7505a7ef0d26d3e0a6
toshi-bp <yttoshi-bp@outlook.jp>
2020-11-11 16:20:34 +0900

    ess完了#9

--- pages/kikaku/105.vue
+++ pages/kikaku/105.vue
@@ -21,6 +21,21 @@
           <TheSection>
             <h3>企画紹介</h3>
             <p class="kikaku-info__description">{{ KikakuList.description }}</p>
+            <div class="kikaku-info__pdf">
+              <a href="kikaku/105/2020理大祭ドラマ_パンフレット.pdf">ドラマパンフレット</a>
+              <iframe
+                src="kikaku/105/2020理大祭ドラマ_パンフレット.pdf"
+                width="100%"
+                height="100%"
+              />
+            </div>
+            <div class="kikaku-info__link">
+              <LinkToContents
+                :live="true"
+                startTime="15:45"
+                endTime="16:45"
+              ></LinkToContents>
+            </div>
           </TheSection>
         </TheContainer>
 
@@ -41,6 +56,7 @@ import KikakuInfoHeader from '~/components/molecules/KikakuInfoHeader'
 import KikakuInfoBody from '~/components/molecules/KikakuInfoBody'
 import TheSection from '~/components/atoms/TheSection'
 import TheContainer from '~/components/atoms/TheContainer'
+import LinkToContents from '~/components/atoms/LinkToContents'
 
 import KikakuList from '~/kikaku/KikakuList.json'
 import makeHead from '~/utils/makeHead.js'
@@ -50,7 +66,8 @@ export default {
     KikakuInfoHeader,
     KikakuInfoBody,
     TheSection,
-    TheContainer
+    TheContainer,
+    LinkToContents
   },
   props: {
     id: {
@@ -82,6 +99,14 @@ export default {
   background-size: repeat;
   &__description {
     white-space: pre-line;
+  }
+  &__pdf {
+    width: 100%;
+    height: 500px;
+    margin-bottom: 3rem;
+  }
+  &__link {
+    margin-top: 3rem;
   }
 }
 </style>
