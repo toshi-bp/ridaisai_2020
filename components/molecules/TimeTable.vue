abbbd65bd8e2601b985ed1d4cd1434cfdd60b0db
toshi-bp <yttoshi-bp@outlook.jp>
2020-11-14 00:57:48 +0900

    これからタイテの配置を行っていくよ #9

--- components/molecules/TimeTable.vue
+++ components/molecules/TimeTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- ライブ配信のタイムテーブルを作成する(作ってもらった画像を埋め込むかこちらで作成するかは未定) -->
+  <!-- ライブ配信のタイムテーブルを作成する(作ってもらった画像を埋め込むかこちらで作成するかは未定)とりあえずこちらで作成 -->
   <!-- こちらで作成する場合でもそうでなくてもTabを利用して日時毎に表示できるようにしたい -->
   <div class="time-table">
     <TheContainer>
@@ -19,7 +19,32 @@
             <h1 class="time-table__body__title">
               21日(土)
             </h1>
-            <p>ああああああ</p>
+            <div class="time-table__saturday">
+              <div
+                 v-for="time in times"
+                  :key="time.id"
+                class="time-table__time"
+              >
+                <div class="time-table__time__line"></div>
+                <div
+                  class="time-table__time__child"
+                >
+                  {{ time.time }}
+                </div>
+                <div></div>
+              </div>
+              <div
+                v-for="stage in Saturday1311"
+                :key="stage.id"
+                class="time-table__saturday__1311"
+              >
+              <div></div>
+              </div>
+              <div
+                v-for="stage in SaturdayLB"
+                :key="stage.id"
+              ></div>
+            </div>
           </div>
           <div
             v-show="dayTab === 2"
@@ -40,6 +65,8 @@
 import TheSection from '~/components/atoms/TheSection'
 import TheContainer from '~/components/atoms/TheContainer'
 
+import StageList from '~/kikaku/StageList.json'
+
 export default {
   components: {
     TheSection,
@@ -57,7 +84,60 @@ export default {
           id: 2,
           day: 'Sunday'
         }
+      ],
+      times: [
+        {
+          id: 1,
+          time: '10:00'
+        },
+        {
+          id: 2,
+          time: '11:00'
+        },
+        {
+          id: 3,
+          time: '12:00'
+        },
+        {
+          id: 4,
+          time: '13:00'
+        },
+        {
+          id: 5,
+          time: '14:00'
+        },
+        {
+          id: 6,
+          time: '15:00'
+        },
+        {
+          id: 7,
+          time: '16:00'
+        },
+        {
+          id: 8,
+          time: '17:00'
+        },
+        {
+          id: 9,
+          time: '18:00'
+        }
       ]
+    }
+  },
+  computed: {
+    Saturday1311 () {
+      return StageList.filter(item => item.id < 7)
+    },
+    SaturdayLB () {
+      // LBは講義棟を表しています(Lecture Building)
+      return StageList.filter(item => item.id > 6 && item.id < 11)
+    },
+    Sunday1311 () {
+      return StageList.filter(item => item.id > 10 && item.id < 14)
+    },
+    SundayLB () {
+      return StageList.filter(item => item.id > 13)
     }
   }
 }
@@ -65,6 +145,19 @@ export default {
 
 <style lang="scss" scoped>
 .time-table {
-
+  &__time {
+    position: relative;
+    &__child {
+      margin-bottom: 5rem;
+      text-align: center;
+      width: 60px;
+    }
+    &__line {
+      border-top: 1px $theme-color dashed;
+      opacity: 0.7;
+      height: 0;
+      width: 100%;
+    }
+  }
 }
 </style>
