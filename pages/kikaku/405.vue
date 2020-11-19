0d635a3581cb119a26a5516d4392a0af793ed608
toshi-bp <yttoshi-bp@outlook.jp>
2020-11-19 14:30:58 +0900

    飛行船企画の作成を進めている #10

---
+++ pages/kikaku/405.vue
@@ -0,0 +1,84 @@
+<template>
+  <!-- ペーパークラフト企画 -->
+  <div class="kikaku-info">
+    <div>
+      <div>
+        <KikakuInfoHeader
+          :id="KikakuList.kikaku_id"
+          :kikaku_name="KikakuList.kikaku_title"
+          :circle_name="KikakuList.name"
+          :type="KikakuList.type"
+          :live="KikakuList.live"
+          :youtube="KikakuList.youtube"
+          :website="KikakuList.website"
+        />
+
+        <!-- ここにコンテンツを掲載するよ -->
+        <TheContainer>
+          <TheSection>
+            <h3>企画紹介</h3>
+            <p class="kikaku-info__description">{{ KikakuList.description }}</p>
+          </TheSection>
+        </TheContainer>
+
+        <KikakuInfoBody
+          :image-url="require(`@/assets/kikaku/${KikakuList.image_filename}`)"
+          :name="KikakuList.name"
+          :introduce="KikakuList.introduction"
+          :url="KikakuList.url"
+          :twitter="KikakuList.twitter_ids"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import KikakuInfoHeader from '~/components/molecules/KikakuInfoHeader'
+import KikakuInfoBody from '~/components/molecules/KikakuInfoBody'
+import TheSection from '~/components/atoms/TheSection'
+import TheContainer from '~/components/atoms/TheContainer'
+
+import KikakuList from '~/kikaku/KikakuList.json'
+import makeHead from '~/utils/makeHead.js'
+
+export default {
+  components: {
+    KikakuInfoHeader,
+    KikakuInfoBody,
+    TheSection,
+    TheContainer
+  },
+  props: {
+    id: {
+      type: Number,
+      default: 210
+    }
+  },
+  computed: {
+    Kikaku () {
+      const id = 210 // kikaku_idの値をjsonから調べて直接入力
+      return KikakuList.filter(item => item.kikaku_id === id)
+    }
+  },
+  head () {
+    return makeHead(
+      '情報技術クラブ',
+      '情報技術クラブ',
+      require('~/assets/kikaku/itc.png')
+    )
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.kikaku-info {
+  padding-top: $global-header-height;
+  padding-bottom: 2rem;
+  background-image: url('~@/assets/image/bg.svg');
+  background-size: repeat;
+  &__description {
+    white-space: pre-line;
+  }
+}
+</style>
