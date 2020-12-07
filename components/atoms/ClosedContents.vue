cbf534cf07baee769e593944bd71796a4ca2b8ad
toshi-bp <yttoshi-bp@outlook.jp>
2020-12-07 21:36:51 +0900

    コンテンツを閉じる際の告知を作成する

---
+++ components/atoms/ClosedContents.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="close">
+    <p class="close__text">
+      コンテンツの公開は終了しました。
+    </p>
+    <p class="close__text">
+      ご来場ありがとうございました。
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.close {
+  background-color: $theme-color;
+  padding: 3rem 1rem;
+  display: block;
+  margin-bottom: 1rem;
+  box-shadow: 0 0.25rem 1rem rgba($color: #000000, $alpha: 0.3);
+  transition: 0.15s ease all;
+  border-radius: 5px;
+  &__text {
+    text-align: center;
+    font-family: $sub-font;
+    font-size: 1.2rem;
+  }
+  &:hover {
+    transform: scale(1.03);
+    box-shadow: 0 0.25rem 1rem rgba($color: #000000, $alpha: 0.5);
+  }
+}
+</style>
